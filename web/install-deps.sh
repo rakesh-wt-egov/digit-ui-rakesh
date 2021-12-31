c3b339573a4458055a50cb33192e842fc9a7ab33
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-12-31 08:56:55 +0530

    Update install-deps.sh

--- web/install-deps.sh
+++ web/install-deps.sh
@@ -8,8 +8,8 @@ if [ "$BRANCH" != "develop" ] || [ "$BRANCH" != "master" ]; then
 
   INTERNALS="digit-ui-internals"
   rm -rf $INTERNALS
-  git clone -b fsm-2.5.1 https://github.com/egovernments/digit-ui-internals.git $INTERNALS
-  cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && yarn && yarn build && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \;
+  git clone -b fsm-fix https://github.com/egovernments/digit-ui-internals.git $INTERNALS
+  cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && yarn install --force  && yarn build && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \;
   cd ..
 
   rm -rf node_modules
