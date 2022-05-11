7268f79801076432e601490d247768df85566f83
Saumya-egov <74046603+saumya-egov@users.noreply.github.com>
2022-05-11 16:29:11 +0530

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -59,7 +59,7 @@ const moduleReducers = (initData) => ({
 function App() {
   customizations.forEach((setup) => setup());
   // const stateCode = window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") || process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
-  const stateCode = "od";
+  const stateCode = "pg";
   if (!stateCode) {
     return <h1>stateCode is not defined</h1>
   }
